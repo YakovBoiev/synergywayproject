4eb35d41ce832fa15cdcd94ed2a082ec95bd8748
YakovBoiev <iakovboiev@gmail.com>
2022-04-03 17:24:39 +0100

    deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -83,11 +83,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'synergywayapp',
+        'USER': 'yakov',
+        'PASSWORD': 'qwerty123',
+        'HOST': 'db',
+        'PORT': '5432',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '24322',
     }
 }
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
